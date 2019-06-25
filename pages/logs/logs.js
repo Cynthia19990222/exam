70ce31c88195fcd9c9559f0d0293266180cd8da3
Cynthia19990222 <zxcccc987@qq.com>
2019-06-25 11:42:29 +0800

    改进版

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -13,3 +13,17 @@ Page({
     })
   }
 })
+//logs.js
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(function (log) {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
